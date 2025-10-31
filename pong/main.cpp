05b782e2c2225b5416ef237c888089c0219eae69
henapbc9da <ars1090ya@gmail.com>
2025-10-31 12:48:54 +0700

    5.3 it (somewhat) works

--- pong/main.cpp
+++ pong/main.cpp
@@ -7,6 +7,8 @@
 #include <ctime>
 #include <cstdlib>
 
+const float pi = 3.14159;
+
 struct {
     HWND hWnd;//хэндл окна
     HDC device_context, context;// два контекста устройства (для буферизации)
@@ -29,8 +31,48 @@ void InitWindow()
 
 }
 
-void DrawLine(int x0, int y0, int z0, int x1, int y1, int z1)
+class Vector2 // a 2-dimensional vector
 {
+public:
+    float x, y;
+    Vector2()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    Vector2(float x0, float y0)
+    {
+        x = x0;
+        y = y0;
+    }
+};
+
+class Vector3 // a 3-dimensional vector
+{
+public:
+    float x, y, z;
+    Vector3()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    Vector3(float x0, float y0, float z0)
+    {
+        x = x0;
+        y = y0;
+        z = z0;
+    }
+};
+
+void DrawLine(Vector2 A, Vector2 B, Vector3 color) // draws a line from point A(x0, y0) to point B(x1, y1) of color RGB(r, g, b)
+{
+    int x0 = A.x;
+    int y0 = A.y;
+    int x1 = B.x;
+    int y1 = B.y;
     int deltax = abs(x1 - x0);
     int deltay = abs(y1 - y0);
     int error = 0;
@@ -60,7 +102,7 @@ void DrawLine(int x0, int y0, int z0, int x1, int y1, int z1)
         {
             for (int x = x0; x < x1; x++)
             {
-                SetPixel(window.context, x, y, RGB(255, 255, 255));
+                SetPixel(window.context, x, y, RGB(color.x, color.y, color.z));
                 error += deltaerr;
                 if (error >= deltax + 1)
                 {
@@ -73,7 +115,7 @@ void DrawLine(int x0, int y0, int z0, int x1, int y1, int z1)
         {
             for (int x = x0; x > x1; x--)
             {
-                SetPixel(window.context, x, y, RGB(255, 255, 255));
+                SetPixel(window.context, x, y, RGB(color.x, color.y, color.z));
                 error += deltaerr;
                 if (error >= deltax + 1)
                 {
@@ -90,7 +132,7 @@ void DrawLine(int x0, int y0, int z0, int x1, int y1, int z1)
         {
             for (int y = y0; y < y1; y++)
             {
-                SetPixel(window.context, x, y, RGB(255, 255, 255));
+                SetPixel(window.context, x, y, RGB(color.x, color.y, color.z));
                 error += deltaerr;
                 if (error >= deltay + 1)
                 {
@@ -103,7 +145,7 @@ void DrawLine(int x0, int y0, int z0, int x1, int y1, int z1)
         {
             for (int y = y0; y > y1; y--)
             {
-                SetPixel(window.context, x, y, RGB(255, 255, 255));
+                SetPixel(window.context, x, y, RGB(color.x, color.y, color.z));
                 error += deltaerr;
                 if (error >= deltay + 1)
                 {
@@ -115,23 +157,100 @@ void DrawLine(int x0, int y0, int z0, int x1, int y1, int z1)
     }
 }
 
+Vector3 RotateVector(Vector3 point, char axis, float angle) // rotates a point in a 3-dimensional coordinate system around either of 3 axis (x, y, z) by an angle (in rad)
+{
+    if (axis == 'x')
+    {
+        float rotationMatrix[3][3] =
+        {
+            {1, 0, 0},
+            {0, cos(angle), -sin(angle)},
+            {0, sin(angle), cos(angle)}
+        };
+        return Vector3(point.x * rotationMatrix[0][0] + point.y * rotationMatrix[1][0] + point.z * rotationMatrix[2][0],
+                       point.x * rotationMatrix[0][1] + point.y * rotationMatrix[1][1] + point.z * rotationMatrix[2][1],
+                       point.x * rotationMatrix[0][2] + point.y * rotationMatrix[1][2] + point.z * rotationMatrix[2][2]);
+    }
+    else if (axis == 'y')
+    {
+        float rotationMatrix[3][3] =
+        {
+            {cos(angle), 0, sin(angle)},
+            {0, 1, 0},
+            {-sin(angle), 0, cos(angle)}
+        };
+        return Vector3(point.x * rotationMatrix[0][0] + point.y * rotationMatrix[1][0] + point.z * rotationMatrix[2][0],
+                       point.x * rotationMatrix[0][1] + point.y * rotationMatrix[1][1] + point.z * rotationMatrix[2][1],
+                       point.x * rotationMatrix[0][2] + point.y * rotationMatrix[1][2] + point.z * rotationMatrix[2][2]);
+    }
+    else if (axis == 'z')
+    {
+        float rotationMatrix[3][3] =
+        {
+            {cos(angle), -sin(angle), 0},
+            {sin(angle), cos(angle), 0},
+            {0, 0, 1}
+        };
+        return Vector3(point.x * rotationMatrix[0][0] + point.y * rotationMatrix[1][0] + point.z * rotationMatrix[2][0],
+                       point.x * rotationMatrix[0][1] + point.y * rotationMatrix[1][1] + point.z * rotationMatrix[2][1],
+                       point.x * rotationMatrix[0][2] + point.y * rotationMatrix[1][2] + point.z * rotationMatrix[2][2]);
+    }
+}
+
+Vector2 crd2scr(Vector3 O) // converts the point's coordinates in my coordinate system to the screen's one (Z doesn't do anything atm)
+{
+    return Vector2(O.x + 960, -O.y + 540);
+}
+
+class Rect // a rectangle which can be drawn on screen by using its draw() method
+{
+public:
+    Vector3 vert1, vert2, vert3, vert4;
+    Rect()
+    {
+        vert1 = Vector3(-window.width / 4, window.height / 4, 0);
+        vert2 = Vector3(window.width / 4, window.height / 4, 0);
+        vert3 = Vector3(window.width / 4, -window.height / 4, 0);
+        vert4 = Vector3(-window.width / 4, -window.height / 4, 0);
+    } 
+
+    Rect(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
+    {
+        vert1 = v1;
+        vert2 = v2;
+        vert3 = v3;
+        vert4 = v4;
+    }
+    void rotate(char axis, float angle)
+    {
+        vert1 = RotateVector(vert1, axis, angle);
+        vert2 = RotateVector(vert2, axis, angle);
+        vert3 = RotateVector(vert3, axis, angle);
+        vert4 = RotateVector(vert4, axis, angle);
+    }
+    void draw()
+    {
+        DrawLine(crd2scr(vert1), crd2scr(vert2), Vector3(255, 255, 255));
+        DrawLine(crd2scr(vert2), crd2scr(vert3), Vector3(255, 255, 255));
+        DrawLine(crd2scr(vert3), crd2scr(vert4), Vector3(255, 255, 255));
+        DrawLine(crd2scr(vert4), crd2scr(vert1), Vector3(255, 255, 255));
+    }
+};
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR    lpCmdLine,
     _In_ int       nCmdShow)
 {
-    const int fps = 60;
+    const int fps = 2;
     InitWindow();//здесь инициализируем все что нужно для рисования в окне
     ShowCursor(NULL);
-    
+    Rect myRect;
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
         BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);//копируем буфер в окно
-        DrawLine(860, 440, 0, 1060, 440, 0);
-        DrawLine(1060, 440, 0, 1060, 640, 0);
-        DrawLine(1060, 640, 0, 860, 640, 0);
-        DrawLine(860, 640, 0, 860, 440, 0);
+        myRect.draw();
+        myRect.rotate('z', pi / 6);
         Sleep(1000. / fps);//ждем 16 милисекунд (1/количество кадров в секунду)
     }
-
 }
